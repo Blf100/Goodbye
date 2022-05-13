169e63f84796d537fdc34377e407ab778cf59df0
Bruno Lopes <brunofabricio14@hotmail.com>
2022-05-13 17:00:24 -0300

    Back-end do projeto completo

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/_H2R1iUcW-5G47PYg-G4UsIlWJAdIp2o",
+      accounts: ["a85e2515e34afb9dbc9ab822f48bf35d1e22c9f2c1155d3d06b38520abe3ed23"],
+    },
+  },
 };
